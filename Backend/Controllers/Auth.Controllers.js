76a64619b6e303096e7615ec4169e4ab3cd1c3ea
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2026-03-19 20:13:59 +0530

    Update Auth.Controllers.js

--- Backend/Controllers/Auth.Controllers.js
+++ Backend/Controllers/Auth.Controllers.js
@@ -67,8 +67,8 @@ export const registerUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 24 * 60 * 60 * 1000,
-      secure: false,
-      sameSite: "none",
+      secure: true,
+      sameSite: "None",
     });
   
     res.status(201).json({
@@ -99,8 +99,8 @@ export const loginUser = async(req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 15 * 24 * 60 * 60 * 1000,
-            secure: false,
-            sameSite: "none",
+            secure: true,
+      sameSite: "None",
           });
         res.status(200).json({ message: "Login successfully" });  
     } catch (error) {
