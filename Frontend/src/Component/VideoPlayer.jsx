98d664dbdafb65b16f77cabe5b33d0a139ef79bc
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2026-03-19 19:23:58 +0530

    Update BASE_URL for video streaming service

--- Frontend/src/Component/VideoPlayer.jsx
+++ Frontend/src/Component/VideoPlayer.jsx
@@ -6,7 +6,7 @@ const VideoPlayer = ({ videoId }) => {
   const videoRef = useRef(null);
   const playerRef = useRef(null);
   // const BASE_URL = "http://localhost:5000/streams";
-  const BASE_URL = "https://videohub-7vd0.onrender.com/streams";
+  const BASE_URL = "https://youtube-backend-n2na.onrender.com/streams";
 
   const availableResolutions = [
     { label: "360p", src: `${BASE_URL}/${videoId}/360p/index.m3u8` },
