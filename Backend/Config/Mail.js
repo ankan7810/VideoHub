98633a859ac16abd81acc817082ad51d5910c954
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2026-03-20 08:55:20 +0530

    Change mail transport security to false

--- Backend/Config/Mail.js
+++ Backend/Config/Mail.js
@@ -8,7 +8,7 @@ dotenv.config({
 const transporter = nodemailer.createTransport({
   host:'smtp.gmail.com',
   port:587,
-  secure:true,
+  secure:false,
   auth:{
     user:process.env.EMAIL,
     pass:process.env.EMAIL_PASSWORD
