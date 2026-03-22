5679f129cfb9ffb425e776eb8af3bd85fe7d5276
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2026-03-22 12:10:37 +0530

    Simplify dotenv configuration

--- Backend/Middlewares/Cloudinary.js
+++ Backend/Middlewares/Cloudinary.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./.env" });
+dotenv.config()
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
