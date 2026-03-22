7ec2b02af79dc4d17be3be511c06654c2163ba45
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2026-03-22 12:08:51 +0530

    Simplify dotenv configuration in Mail.js
    Simplified dotenv configuration by removing path.

--- Backend/Config/Mail.js
+++ Backend/Config/Mail.js
@@ -2,9 +2,7 @@ import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 import dns from "dns";
 
-dotenv.config({
-    path:'./.env'
-});
+dotenv.config();
 dns.setDefaultResultOrder("ipv4first");
 const transporter = nodemailer.createTransport({
   service: "Gmail",
